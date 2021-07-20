220a403077b863fbd0a455733b83ed07b5c18203
kabilan <kabilan93@hotmail.sg>
2021-07-21 02:48:25 +0800

    1.8_coditional_check_url_prefix

--- fetch_url.go
+++ fetch_url.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Initial setup of go file")
+	cmdline_arg_url := os.Args[1:] // get access to arguments passed i cmd line when running the project
+
+	for _, url := range cmdline_arg_url {
+		//fmt.Println(item)
+		//1.8
+
+		prefix := "http://"
+
+		if strings.HasPrefix(url, prefix) == false {
+			url = prefix + url
+		}
+
+		resp, err := http.Get(url)
+		// handle error with http get fuction
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		//1.7
+		body, err := io.Copy(os.Stdout, resp.Body)
+
+		// body, err := ioutil.ReadAll(resp.Body)
+		// resp.Body.Close()
+		//handle error with reading response body
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf(string(rune(body)))
+	}
 }
