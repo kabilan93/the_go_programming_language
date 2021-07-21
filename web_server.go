a72c81719d6e5dc074810447d0a2ca55f43aec69
kabilan <kabilan93@hotmail.sg>
2021-07-22 02:49:17 +0800

    minimal web server implementation

--- web_server.go
+++ web_server.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Initial setup")
+
+	http.HandleFunc("/", handler) //each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	//fmt.Println("Initial setup")
+}
+
+//handler echoes the Path component of the requested URL.
+// request is recieved as a struct
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Patch = %q\n", r.URL.Path)
 }
