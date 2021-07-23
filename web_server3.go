9774b7105ae7c3abc8b17518e3fcdb8535dea48e
kabilan <kabilan93@hotmail.sg>
2021-07-23 12:36:53 +0800

    1.12 lissajous cycle input from get form

--- web_server3.go
+++ web_server3.go
@@ -10,6 +10,7 @@ import (
 	"math"
 	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 var palette = []color.Color{color.White, color.Black, color.RGBA{0x00, 0xFF, 0x00, 0xFF}}
@@ -22,43 +23,38 @@ const (
 
 func main() {
 
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		lissajous(w)
-	}
 	http.HandleFunc("/", handler) //each request calls handler
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
-	//fmt.Println("Initial setup")
+
 }
 
 //handler echoes the Path component of the requested URL.
 // request is recieved as a struct
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
-
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-	}
-
-	fmt.Fprintf(w, "HOST = %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
 	if err := r.ParseForm(); err != nil {
 		log.Print(err)
 	}
-	for k, v := range r.Form {
-		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	if len(r.Form["cycles"]) == 0 {
+		fmt.Fprintf(w, "error: no cycles parameter")
+		return
 	}
+	var cyclesString string = r.Form["cycles"][0]
+	cycles, err := strconv.Atoi(cyclesString)
+	if err != nil {
+		fmt.Fprintf(w, "error parsing cycles: %v", err)
+		return
+	}
+	lissajous(w, float64(cycles))
 }
 
-func lissajous(out io.Writer) {
+func lissajous(out io.Writer, cycles float64) {
 	const (
-		cycles  = 5     // no. of complete x oscillator revolutions
 		res     = 0.001 // angular resolution
 		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // no. of animation frames
-		delay   = 8     // delay between frames in 10ms unit
+		nframes = 64    // number of animations
+		delay   = 8     // delay between frames in 10ms units
 	)
-
-	freq1 := rand.Float64() * 3.0 //relative frequency of y oscillator
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
 	for i := 0; i < nframes; i++ {
@@ -66,15 +62,12 @@ func lissajous(out io.Writer) {
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
-			y := math.Sin(t*freq1 + phase)
-
-			index := rand.Intn(2) + 1
-
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(index))
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
 	}
-	gif.EncodeAll(out, &anim) // Note: ignoring encoding errors
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
 }
